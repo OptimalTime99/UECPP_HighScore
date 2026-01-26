290238eafcb3c22976c1593bd47ddde33d8ed525
optimal <optimal9744@gmail.com>
2026-01-26 10:05:51 +0900

    Add project files.

---
+++ Source/HighScore/HighScore.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HighScore.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HighScore, "HighScore" );
