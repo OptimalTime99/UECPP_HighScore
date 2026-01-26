c7a59dbe363adf8d3260536c37ec8f50272856ce
optimal <optimal9744@gmail.com>
2026-01-26 19:25:17 +0900

    입력 액션, 입력 매핑 컨텍스트 추가 및 PlayerController에 연결

--- Source/HighScore/Public/HighScorePlayerController.h
+++ Source/HighScore/Public/HighScorePlayerController.h
@@ -1,10 +1,13 @@
-// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "HighScorePlayerController.generated.h"
+
+class UInputMappingContext; // IMC 관련 전방 선언
+class UInputAction; // IA 관련 전방 선언
 
 /**
  * 
@@ -13,5 +16,26 @@ UCLASS()
 class HIGHSCORE_API AHighScorePlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AHighScorePlayerController();
+
+	// 에디터에서 세팅할 IMC
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+	// IA_Move를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	// IA_Jump를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	// IA_Look를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+	// IA_Sprint를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> SprintAction;
+
+protected:
+	virtual void BeginPlay() override;
 };
