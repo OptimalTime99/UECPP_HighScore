85c520d5e10a092a7a73f5a5f6aa18074775dc95
optimal <optimal9744@gmail.com>
2026-01-26 10:21:33 +0900

    HighScoreGameMode 클래스 생성 및 프로젝트 설정에 BP 적용

---
+++ Source/HighScore/Private/HighScoreGameMode.cpp
@@ -0,0 +1,2 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+#include "HighScoreGameMode.h"
---
+++ Source/HighScore/Public/HighScoreGameMode.h
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "HighScoreGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIGHSCORE_API AHighScoreGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
