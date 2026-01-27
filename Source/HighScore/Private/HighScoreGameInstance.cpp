e5c8e8988ff10f9b9d727b7f51f9581547a16bf0
OptimalTime99 <optimal9744@gmail.com>
2026-01-27 17:12:41 +0900

    Game Instance를 활용한 데이터 유지하기

---
+++ Source/HighScore/Private/HighScoreGameInstance.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+
+#include "HighScoreGameInstance.h"
+
+UHighScoreGameInstance::UHighScoreGameInstance()
+{
+    TotalScore = 0;
+    CurrentLevelIndex = 0;
+}
+
+void UHighScoreGameInstance::AddToScore(int32 Amount)
+{
+    TotalScore += Amount;
+    UE_LOG(LogTemp, Warning, TEXT("Total Score Updated: %d"), TotalScore);
+}
