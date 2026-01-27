492c8e5a5dfa21941de5f3c524fed70cf04d28e7
optimal <optimal9744@gmail.com>
2026-01-27 12:57:25 +0900

    박스 콜리전 컴포넌트로 스폰 영역 정하고 랜덤 위치에 아이템 스폰하기

---
+++ Source/HighScore/Private/Item/SpawnVolume.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+
+#include "Item/SpawnVolume.h"
+#include "Components/BoxComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+ASpawnVolume::ASpawnVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    // 박스 컴포넌트를 생성하고, 이 액터의 루트로 설정
+    Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+    SetRootComponent(Scene);
+
+    SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
+    SpawningBox->SetupAttachment(Scene);
+}
+
+FVector ASpawnVolume::GetRandomPointInVolume() const
+{
+    // 1) 박스 컴포넌트의 스케일된 Extent, 즉 x/y/z 방향으로 반지름(절반 길이)을 구함
+    FVector BoxExtent = SpawningBox->GetScaledBoxExtent();
+    // 2) 박스 중심 위치
+    FVector BoxOrigin = SpawningBox->GetComponentLocation();
+
+    // 3) 각 축별로 -Extent ~ +Extent 범위의 무작위 값 생성
+    return BoxOrigin + FVector(
+        FMath::FRandRange(-BoxExtent.X, BoxExtent.X),
+        FMath::FRandRange(-BoxExtent.Y, BoxExtent.Y),
+        FMath::FRandRange(-BoxExtent.Z, BoxExtent.Z)
+    );
+}
+
+void ASpawnVolume::SpawnItem(TSubclassOf<AActor> ItemClass)
+{
+    if (!ItemClass) return;
+
+    GetWorld()->SpawnActor<AActor>(
+        ItemClass,
+        GetRandomPointInVolume(),
+        FRotator::ZeroRotator
+    );
+}
---
+++ Source/HighScore/Public/Item/SpawnVolume.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class HIGHSCORE_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+    ASpawnVolume();
+
+    // 스폰 볼륨 내부에서 무작위 좌표를 얻어오는 함수
+    UFUNCTION(BlueprintCallable, Category = "Spawning")
+    FVector GetRandomPointInVolume() const;
+
+    // 특정 아이템 클래스를 스폰하는 함수
+    UFUNCTION(BlueprintCallable, Category = "Spawning")
+    void SpawnItem(TSubclassOf<AActor> ItemClass);
+
+private:
+    UPROPERTY(Category = "Spawning", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USceneComponent> Scene;
+    
+    // 스폰 영역을 담당할 박스 컴포넌트
+    UPROPERTY(Category = "Spawning", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UBoxComponent> SpawningBox;
+};
