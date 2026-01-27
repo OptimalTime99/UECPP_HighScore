244b20a42f3f9d1e81005f6d705b117f93cee121
optimal <optimal9744@gmail.com>
2026-01-27 11:53:36 +0900

    BaseItem에 충돌 컴포넌트 생성 후 이벤트 바인딩, 충돌 로직 추가

--- Source/HighScore/Private/Item/BaseItem.cpp
+++ Source/HighScore/Private/Item/BaseItem.cpp
@@ -2,28 +2,64 @@
 
 
 #include "Item/BaseItem.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 ABaseItem::ABaseItem()
 {
 	// 틱(매 프레임 실행)은 필요 없으므로 끕니다.
 	PrimaryActorTick.bCanEverTick = false;
+
+	// 루트 컴포넌트 생성 및 설정
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SetRootComponent(Scene);
+
+	// 충돌 컴포넌트 생성 및 설정
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	// 겹침만 감지하는 프로파일 설정
+	Collision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	// 루트 컴포넌트로 설정
+	Collision->SetupAttachment(Scene);
+
+	// 스태틱 메시 컴포넌트 생성 및 설정
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(Collision);
+	// 메시가 불필요하게 충돌을 막지 않도록 하기 위해, 별도로 NoCollision 등으로 설정할 수 있음.
 }
 
 void ABaseItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Overlap 이벤트 바인딩
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &ABaseItem::OnItemOverlap);
+	Collision->OnComponentEndOverlap.AddDynamic(this, &ABaseItem::OnItemEndOverlap);
 }
 
 // 플레이어가 아이템 범위에 들어왔을 때 동작
-void ABaseItem::OnItemOverlap(AActor* OverlapActor)
+void ABaseItem::OnItemOverlap(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
 {
-	// 기본은 빈 함수 - 각 자식 클래스에서 구현
+	// OtherActor가 플레이어인지 확인 ("Player" 태그 활용)
+	if (OtherActor && OtherActor->ActorHasTag("Player"))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Overlap!!!")));
+		// 아이템 사용 (획득) 로직 호출
+		ActivateItem(OtherActor);
+	}
 }
 
 // 플레이어가 아이템 범위를 벗어났을 때 동작
-void ABaseItem::OnItemEndOverlap(AActor* OverlapActor)
+void ABaseItem::OnItemEndOverlap(
+	UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
 {
 	// 기본은 빈 함수 - 필요하다면 자식 클래스에서 활용
 }
@@ -31,13 +67,13 @@ void ABaseItem::OnItemEndOverlap(AActor* OverlapActor)
 // 아이템이 사용(Activate)되었을 때 동작
 void ABaseItem::ActivateItem(AActor* Activator)
 {
-	// 기본은 빈 함수 - 자식 클래스에서 구현
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Overlap!!")));
 }
 
 // 아이템 유형을 반환
 FName ABaseItem::GetItemType() const
 {
-	return FName();
+	return ItemType;
 }
 
 // 아이템을 파괴(제거)하는 함수
--- Source/HighScore/Public/Item/BaseItem.h
+++ Source/HighScore/Public/Item/BaseItem.h
@@ -7,6 +7,8 @@
 #include "ItemInterface.h"
 #include "BaseItem.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class HIGHSCORE_API ABaseItem : public AActor, public IItemInterface
 {
@@ -23,12 +25,33 @@ protected:
     virtual void BeginPlay() override;
 
     // IItemInterface에서 요구하는 함수들을 반드시 구현
-    virtual void OnItemOverlap(AActor* OverlapActor) override;
-    virtual void OnItemEndOverlap(AActor* OverlapActor) override;
+    virtual void OnItemOverlap(
+        UPrimitiveComponent* OverlappedComp,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult) override;
+    virtual void OnItemEndOverlap(
+        UPrimitiveComponent* OverlappedComp,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex) override;
     virtual void ActivateItem(AActor* Activator) override;
     virtual FName GetItemType() const override;
 
     // 아이템을 제거하는 공통 함수 (추가 이펙트나 로직을 넣을 수 있음)
     virtual void DestroyItem();
+
+private:
+    // 루트 컴포넌트 (씬)
+    UPROPERTY(Category = "Item|Component", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USceneComponent> Scene;
+    // 충돌 컴포넌트 (플레이어 진입 범위 감지용)
+    UPROPERTY(Category = "Item|Component", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<USphereComponent> Collision;
+    // 아이템 시각 표현용 스태틱 메시
+    UPROPERTY(Category = "Item|Component", VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UStaticMeshComponent> StaticMesh;
 
 };
