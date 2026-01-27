c704f982353f66793b1bd7fda0e0cdf7f5db5758
optimal <optimal9744@gmail.com>
2026-01-27 11:26:29 +0900

    BaseItem을 상속받은 CoinItem 클래스 생성

---
+++ Source/HighScore/Private/Item/CoinItem.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+
+#include "Item/CoinItem.h"
+
+ACoinItem::ACoinItem()
+{
+    // 부모 생성자 로직 (필요 시)
+}
---
+++ Source/HighScore/Public/Item/CoinItem.h
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/BaseItem.h"
+#include "CoinItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIGHSCORE_API ACoinItem : public ABaseItem
+{
+    GENERATED_BODY()
+
+public:
+    ACoinItem();
+
+protected:
+    // 코인 획득 시 얻을 점수 (자식 클래스에서 상속받아 값만 바꿔줄 예정)
+    UPROPERTY(Category = "Item", EditAnywhere, BlueprintReadWrite)
+    int32 PointValue;
+    
+};
