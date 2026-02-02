f66e506915ab9617c965f9e0798e8540640c1f33
OptimalTime99 <optimal9744@gmail.com>
2026-02-02 16:38:56 +0900

    블루프린트 버튼 이벤트 → C++ UserWidget을 상속받은 MainMenuWidget으로 클래스로 변경

---
+++ Source/HighScore/Private/Widget/MainMenuWidget.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+
+#include "Widget/MainMenuWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+#include "Character/HighScorePlayerController.h"
+
+void UMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 버튼 클릭 이벤트에 함수 바인딩
+	if (StartButton)
+	{
+		StartButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnStartClicked);
+	}
+
+	if (ExitButton)
+	{
+		ExitButton->OnClicked.AddDynamic(this, &UMainMenuWidget::OnExitClicked);
+	}
+}
+
+void UMainMenuWidget::OnStartClicked()
+{
+	// 1. 현재 플레이어 컨트롤러를 가져와서 해당 클래스로 캐스팅
+	AHighScorePlayerController* PC = Cast<AHighScorePlayerController>(GetOwningPlayer());
+
+	if (PC)
+	{
+		// 2. 이미 구현되어 있는 StartGame 함수를 바로 호출
+		PC->StartGame();
+	}
+}
+
+void UMainMenuWidget::OnExitClicked()
+{
+	AHighScorePlayerController* PC = Cast<AHighScorePlayerController>(GetOwningPlayer());
+
+	if (PC)
+	{
+		// 3. 이미 구현되어 있는 ExitGame 함수를 바로 호출
+		PC->ExitGame();
+	}
+}
+
---
+++ Source/HighScore/Public/Widget/MainMenuWidget.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2026 Junhyeok Choi. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIGHSCORE_API UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	// 블루프린트의 버튼 이름과 정확히 일치해야 합니다. (StartButton, ExitButton)
+	UPROPERTY(meta = (BindWidget))
+	class UButton* StartButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* ExitButton;
+
+	// 위젯 초기화 시 실행 (NativeConstruct는 BeginPlay와 유사함)
+	virtual void NativeConstruct() override;
+
+	// 버튼 클릭 시 호출될 함수
+	UFUNCTION()
+	void OnStartClicked();
+
+	UFUNCTION()
+	void OnExitClicked();
+};
